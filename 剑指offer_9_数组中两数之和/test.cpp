dc7a70b013bc9ba98254f576cfbc0adb6ae7706b
浅忆梦微凉 <3538922590@qq.com>
2019-01-13 21:17:51 +0800

    Update test.cpp

--- 剑指offer_9_数组中两数之和/test.cpp
+++ 剑指offer_9_数组中两数之和/test.cpp
@@ -6,23 +6,23 @@ using namespace std;
 //输入一个递增排序的数组和一个数字S，在数组中查找两个数，使得他们的和正好是S，
 //如果有多对数字的和等于S，输出两个数的乘积最小的
 
-//class Solution1 {
-//public:
-//	vector<int> FindNumbersWithSum(vector<int> array, int sum) {
-//		vector<int> result;
-//		int i = 0;
-//		int j = 0;
-//		for (i = 1; i < array.size(); ++i){
-//			for (j = 0; j < i; ++j){
-//				if (sum == (array[i] + array[j])){
-//					result.push_back(array[j]);
-//					result.push_back(array[i]);
-//					return result;
-//				}
-//			}
-//		}
-//	}
-//};
+class Solution1 {
+public:
+	vector<int> FindNumbersWithSum(vector<int> array, int sum) {
+		vector<int> result;
+		int i = 0;
+		int j = 0;
+		for (i = 1; i < array.size(); ++i){
+			for (j = 0; j < i; ++j){
+				if (sum == (array[i] + array[j])){
+					result.push_back(array[j]);
+					result.push_back(array[i]);
+					return result;
+				}
+			}
+		}
+	}
+};
 
 class Solution2 {
 public:
